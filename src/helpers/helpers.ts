f9433a58aae18bc0eb59c2459d64b8d3f355c70b
Kevin Martins <k.martins@newrest-wagonslits.eu>
2025-05-06 17:04:29 +0200

    feat: add color on specific difficulty or gameMode

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,6 @@
 import { questions } from "../data/questions"
 import { AnswersModel } from "../models/answers"
+import { DifficultiesModel } from "../models/difficulties"
 import { QuestionModel } from "../models/questions"
 import { SettingsModels } from "../models/settings"
 
@@ -33,4 +34,38 @@ export const getAllQuestionsById = (questions: QuestionModel[], answers: Answers
     acc.push({ ...question, isCorrect })
     return acc
   }, [])
+}
+
+export const setGameModeColor = (gameMode: string) => {
+  switch (gameMode) {
+    case "Classique":
+      return "bg-blue-300"
+    case "Contre la montre":
+      return "bg-fuchsia-300"
+    case "Sans faute":
+      return "bg-indigo-300"
+    case "Vrai ou Faux":
+      return "bg-lime-300"
+    case "Survie":
+      return "bg-cyan-300"
+    case "Masqué":
+      return "bg-orange-300"
+    default:
+      return ""
+  }
+}
+
+export const setDifficultyColor = (difficulty: DifficultiesModel) => {
+  switch (difficulty as DifficultiesModel) {
+    case "Facile":
+      return "bg-green-300"
+    case "Moyen":
+      return "bg-yellow-300"
+    case "Difficile":
+      return "bg-red-300"
+    case "Extrême":
+      return "bg-purple-300"
+    default:
+      return ""
+  }
 }
