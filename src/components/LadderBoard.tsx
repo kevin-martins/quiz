56cb9bab0084438f1cd321035575759fbc4e88db
Kevin Martins <k.martins@newrest-wagonslits.eu>
2025-05-06 17:05:04 +0200

    feat: move ladderboard to its own component

---
+++ src/components/LadderBoard.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useAppSelector } from "../app/hooks";
+import { RootState } from "../app/store";
+import { LadderBoardType } from "../models/ladderBoard";
+import { getAllQuestionsById } from "../helpers/helpers";
+
+const LadderBoard = () => {
+  const { questions, currentQuestion, points, answers } = useAppSelector((state: RootState) => state.game)
+  const [ladderBoard, setLadderBoard] = useState<LadderBoardType>(LadderBoardType.ANSWERS)
+
+  return (
+    <div className='w-[1024px] m-auto border-[1px] border-gray-300 rounded-lg p-1 mt-4'>
+      <div className='grid grid-cols-2 mb-3 rounded-lg'>
+        <button
+          onClick={() => setLadderBoard(LadderBoardType.ANSWERS)}
+          className={`rounded-tl-lg py-2 ${
+            ladderBoard === LadderBoardType.ANSWERS ? "bg-gray-300" : "hover:bg-gray-200 cursor-pointer"}`}
+        >
+          Réponses
+        </button>
+        <button
+          onClick={() => setLadderBoard(LadderBoardType.STATISTICS)}
+          className={`rounded-tr-lg py-2 ${
+            ladderBoard === LadderBoardType.STATISTICS ? "bg-gray-300" : "hover:bg-gray-200 cursor-pointer"}`}
+        >
+          Statistiques
+        </button>
+      </div>
+      {ladderBoard === LadderBoardType.ANSWERS &&
+        <div className='flex flex-col gap-[2px]'>
+          {getAllQuestionsById(questions, answers).map((answer) => {
+            return (
+              <p
+                className={`rounded px-2 w-max ${answer.isCorrect ? "bg-green-200" : "bg-red-200"}`}
+              >
+                {answer.question}
+              </p>
+            )
+          })}
+        </div>
+      }
+      {ladderBoard === LadderBoardType.STATISTICS &&
+        <div className='flex flex-col gap-[2px]'>
+          <p>Nombre de réponses: {answers.length}</p>
+          <p>Bonnes réponses: {answers.reduce((acc, curr) => { return curr.isCorrect ? acc += 1 : acc }, 0)}</p>
+          <p>Mauvaises réponses: {answers.reduce((acc, curr) => { return !curr.isCorrect ? acc += 1 : acc }, 0)}</p>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default LadderBoard
--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,9 +1,10 @@
 import { RootState } from '../app/store'
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { addAnswer, addPoints, newRandomQuestion } from '../features/gameSlice'
-import { getAllQuestionsById } from '../helpers/helpers'
+import { getAllQuestionsById, setDifficultyColor, setGameModeColor } from '../helpers/helpers'
 import { LadderBoardType } from '../models/ladderBoard'
 import { useState } from 'react'
+import LadderBoard from './LadderBoard'
 
 const Questions = () => {
   const { categories, difficulties, gameMode } = useAppSelector((state: RootState) => state.settings)
@@ -20,25 +21,25 @@ const Questions = () => {
     dispatch(newRandomQuestion())
   }
 
-  const [ladderBoard, setLadderBoard] = useState<LadderBoardType>(LadderBoardType.ANSWERS)
-
   return (
-    <div>
-      <span>Mode de jeu {gameMode}</span>
-      <span>Difficulté: {difficulties}</span>
+    <div className='flex flex-col gap-4'>
+      <span>Mode de jeu: {gameMode}</span>
+      <span>Difficulté: {difficulties.map(difficulty => <span className={`px-4 py-1 mx-1 ${setDifficultyColor(difficulty)} rounded-lg`}>{difficulty}</span>)}</span>
       <span>Categories: {categories}</span>
       <span>Nombre de questions: {questions.length}</span>
       <span>points {points}</span>
 
       <div className='w-[1024px] m-auto'>
-        <div className='flex'>
-          <span>{currentQuestion.category}</span>
-          <span>{currentQuestion.gameMode}</span>
-          <span>{currentQuestion.difficulty}</span>
+        <div className='flex items-center mb-5'>
+          <h2 className='text-5xl pr-4'>{currentQuestion.category}</h2>
+          <div className='flex flex-row gap-2 mt-auto text-sm'>
+            <p className={`px-4 py-1 rounded-lg ${setGameModeColor(currentQuestion.gameMode)}`}>{currentQuestion.gameMode}</p>
+            <p className={`px-4 py-1 rounded-lg ${setDifficultyColor(currentQuestion.difficulty)}`}>{currentQuestion.difficulty}</p>
+          </div>
         </div>
         <div>
-          <h2>{currentQuestion.question}</h2>
-          <div className='grid grid-cols-2 gap-20'>
+          <h2 className='text-lg'>{currentQuestion.question}</h2>
+          <div className='grid grid-cols-2 gap-5 mt-2'>
             {currentQuestion.choices.map((choice: string) => (
               <button
                 key={choice}
@@ -51,44 +52,7 @@ const Questions = () => {
           </div>
         </div>
       </div>
-
-      <div className='w-[1024px] m-auto border-[1px] border-gray-300 rounded-lg p-1 mt-4'>
-        <div className='grid grid-cols-2 mb-3 rounded-lg'>
-          <button
-            onClick={() => setLadderBoard(LadderBoardType.ANSWERS)}
-            className={`rounded-tl-lg py-2 ${
-              ladderBoard === LadderBoardType.ANSWERS ? "bg-gray-300" : "hover:bg-gray-200 cursor-pointer"}`}
-          >
-            Réponses
-          </button>
-          <button
-            onClick={() => setLadderBoard(LadderBoardType.STATISTICS)}
-            className={`rounded-tr-lg py-2 ${
-              ladderBoard === LadderBoardType.STATISTICS ? "bg-gray-300" : "hover:bg-gray-200 cursor-pointer"}`}
-          >
-            Statistiques
-          </button>
-        </div>
-        {ladderBoard === LadderBoardType.ANSWERS &&
-          <div className='flex flex-col gap-[2px]'>
-            {getAllQuestionsById(questions, answers).map((answer) => {
-              return (
-                <p
-                  className={`rounded px-2 w-max ${answer.isCorrect ? "bg-green-200" : "bg-red-200"}`}
-                >
-                  {answer.question}
-                </p>
-              )
-            })}
-          </div>
-        }
-        {ladderBoard === LadderBoardType.STATISTICS &&
-          <div className='flex flex-col gap-[2px]'>
-            <p>Bonnes réponses: {answers.reduce((acc, curr) => { return curr.isCorrect ? acc += 1 : acc }, 0)}</p>
-            <p>Mauvaises réponses: {answers.reduce((acc, curr) => { return !curr.isCorrect ? acc += 1 : acc }, 0)}</p>
-          </div>
-        }
-      </div>
+      <LadderBoard />
     </div>
   )
 }
